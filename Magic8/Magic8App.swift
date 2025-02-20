c380c0f660712a0875e4b90202775923bc01867c
Linda Dang <lindadang801@gmail.com>
2025-02-20 13:01:47 +1100

    Initial Commit

---
+++ Magic8/Magic8App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8App.swift
+//  Magic8
+//
+//  Created by Linda Dang on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Magic8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
